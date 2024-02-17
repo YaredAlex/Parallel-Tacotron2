4b17635c9d444a7a714fbab676d9d60a731a28c3
YaredAlex <109204023+YaredAlex@users.noreply.github.com>
2024-02-18 02:06:45 +0530

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -60,9 +60,9 @@ def get_vocoder(config, device):
         config = hifigan.AttrDict(config)
         vocoder = hifigan.Generator(config)
         if speaker == "LJSpeech":
-            ckpt = torch.load(new_path+"hifigan/generator_LJSpeech.pth.tar")
+            ckpt = torch.load(new_path+"hifigan/generator_LJSpeech.pth.tar.zip")
         elif speaker == "universal":
-            ckpt = torch.load(new_path+"hifigan/generator_universal.pth.tar")
+            ckpt = torch.load(new_path+"hifigan/generator_universal.pth.tar.zip")
         vocoder.load_state_dict(ckpt["generator"])
         vocoder.eval()
         vocoder.remove_weight_norm()
